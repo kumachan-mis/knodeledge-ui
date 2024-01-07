e60218ffbb78c6f7e9f70fc7ae58711ff2bd95de
kumachan-mis <29433058+kumachan-mis@users.noreply.github.com>
2024-01-07 20:43:06 +0900

    fix: use idTokenProvider.fetchIdToken

--- src/utils/googleAuth.ts
+++ src/utils/googleAuth.ts
@@ -1,9 +1,12 @@
 import { GoogleAuth } from 'google-auth-library';
 
+const TARGET_AUDIENCE = process.env.NEXT_PUBLIC_API_URL ?? '';
+const GOOGLE_AUTH_HEADER = 'X-Serverless-Authorization';
+
 const auth = new GoogleAuth();
 
 export async function fetchWithGoogleAuth(url: string, options?: RequestInit) {
-  const client = await auth.getIdTokenClient(process.env.NEXT_PUBLIC_API_URL ?? '');
-  const headers = await client.getRequestHeaders();
-  return fetch(url, { ...options, headers: { ...options?.headers, ...headers } });
+  const client = await auth.getIdTokenClient(TARGET_AUDIENCE);
+  const idToken = await client.idTokenProvider.fetchIdToken(TARGET_AUDIENCE);
+  return fetch(url, { ...options, headers: { ...options?.headers, [GOOGLE_AUTH_HEADER]: `Bearer ${idToken}` } });
 }
