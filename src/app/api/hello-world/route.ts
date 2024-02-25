d2f0c9d25dad3ea3d41e59c9be8b2ca6e74bdec9
Kumachan <29433058+kumachan-mis@users.noreply.github.com>
2024-02-25 21:07:07 +0900

    feat: remove hello world (#53)

--- src/app/api/hello-world/route.ts
+++
@@ -1,5 +0,0 @@
-import { JSON_ROUTE } from '@/routes/common';
-
-export async function POST(request: Request): Promise<Response> {
-  return JSON_ROUTE('/api/hello-world', 'POST', request);
-}
--- src/openapi/apis/index.ts
+++ src/openapi/apis/index.ts
@@ -1,3 +1,2 @@
 /* eslint-disable */
-export * from './HelloWorldApi';
 export * from './ProjectsApi';
