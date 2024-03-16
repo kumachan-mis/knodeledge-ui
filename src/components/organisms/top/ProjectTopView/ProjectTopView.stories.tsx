7afa204f17ec9031f70b9182d1f5b5dff1c43804
kumachan-mis <29433058+kumachan-mis@users.noreply.github.com>
2024-03-16 18:35:56 +0900

    implement story of ProjectTopView

---
+++ src/components/organisms/top/ProjectTopView/ProjectTopView.stories.tsx
@@ -0,0 +1,67 @@
+import ProjectTopViewComponent from './ProjectTopView';
+
+import { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof ProjectTopViewComponent> = {
+  component: ProjectTopViewComponent,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof ProjectTopViewComponent>;
+
+export const WithoutDescription: Story = {
+  args: {
+    loadableProject: {
+      state: 'success',
+      data: {
+        id: 'PROJECT_WITHOUT_DESCRIPTION',
+        name: 'Project Without Description',
+      },
+      error: null,
+    },
+  },
+};
+
+export const WithDescription: Story = {
+  args: {
+    loadableProject: {
+      state: 'success',
+      data: {
+        id: 'PROJECT_WITH_DESCRIPTION',
+        name: 'Project With Description',
+        description: 'This is my project.',
+      },
+      error: null,
+    },
+  },
+};
+
+export const LongDescription: Story = {
+  args: {
+    loadableProject: {
+      state: 'success',
+      data: {
+        id: 'PROJECT_WITH_LONG_DESCRIPTION',
+        name: 'Project With Long Description',
+        description: [
+          "My project's description is very long.",
+          "It's so long that it's hard to fit it in a single line.",
+          "This long description is a good test for the component's layout.",
+          "This long description is a good test for the component's layout.",
+        ].join(' '),
+      },
+      error: null,
+    },
+  },
+};
+
+export const Loading: Story = {
+  args: {
+    loadableProject: {
+      state: 'loading',
+      data: null,
+      error: null,
+    },
+  },
+};
