83ab5214a61f00592890763a13a956c00d90187f
kumachan-mis <29433058+kumachan-mis@users.noreply.github.com>
2024-05-01 14:14:01 +0900

    fix typecheck

--- src/actions/projects/updateProject.ts
+++ src/actions/projects/updateProject.ts
@@ -12,7 +12,7 @@ export async function updateProject(
   request: ProjectUpdateRequest,
 ): Promise<Errorable<ProjectUpdateResponse, ProjectUpdateErrorResponse>> {
   return await fetchFromOpenApi<ProjectUpdateResponse, ProjectUpdateErrorResponse>(
-    async () => await projectsApi.update({ projectUpdateRequest: request }),
+    async () => await projectsApi.projectsUpdate({ projectUpdateRequest: request }),
     async (error) => {
       if (400 <= error.response.status && error.response.status < 500) {
         const errorResponse = ProjectUpdateErrorResponseFromJSON(await error.response.json());
