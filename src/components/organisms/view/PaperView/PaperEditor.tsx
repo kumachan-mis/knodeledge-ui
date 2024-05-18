b7d346c7f9bfc4fa98fe783258ac0398488661cf
kumachan-mis <29433058+kumachan-mis@users.noreply.github.com>
2024-05-18 22:20:19 +0900

    implement PaperEditorFooter

--- src/components/organisms/view/PaperView/PaperEditor.tsx
+++ src/components/organisms/view/PaperView/PaperEditor.tsx
@@ -2,6 +2,8 @@ import { LoadableAction } from '@/contexts/openapi';
 import { PaperActionError } from '@/contexts/papers';
 import { Paper, PaperWithoutAutofield } from '@/openapi';
 
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 import { styled } from '@mui/material/styles';
 import React from 'react';
 import { EditorRoot, EditorSyntaxMenu, Divider, EditorTextFieldRoot, EditorTextFieldBody } from 'react-clay-editor';
@@ -11,16 +13,18 @@ export type PaperEditorComponentProps = {
   readonly updatePaper: (id: string, paper: PaperWithoutAutofield) => Promise<LoadableAction<PaperActionError>>;
 };
 
-const PaperEditorComponent: React.FC<PaperEditorComponentProps> = ({ paper }) => {
-  const [text, setText] = React.useState(paper.content);
+const PaperEditorComponent: React.FC<PaperEditorComponentProps> = ({ paper, updatePaper }) => {
+  const [content, setContent] = React.useState(paper.content);
 
   return (
-    <PaperEditorRoot setText={setText} text={text}>
+    <PaperEditorRoot setText={setContent} text={content}>
       <EditorSyntaxMenu />
       <Divider />
       <EditorTextFieldRoot>
         <EditorTextFieldBody />
       </EditorTextFieldRoot>
+      <Divider />
+      <PaperEditorFooter onSave={() => void updatePaper(paper.id, { content })} />
     </PaperEditorRoot>
   );
 };
@@ -31,5 +35,13 @@ const PaperEditorRoot = styled(EditorRoot)(({ theme }) => ({
     height: `calc(100vh - ${theme.mixins.toolbar.minHeight}px - ${theme.spacing(2)})`,
   },
 }));
+
+const PaperEditorFooter: React.FC<{ readonly onSave: () => void }> = ({ onSave }) => (
+  <Box sx={{ width: '100%', display: 'flex', justifyContent: 'flex-end' }}>
+    <Button color="primary" onClick={onSave} size="small" sx={{ m: 1 }} variant="contained">
+      Save
+    </Button>
+  </Box>
+);
 
 export default PaperEditorComponent;
