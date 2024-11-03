1bb7d1692c72c2067a5dcf01bc93fb183dbf31b8
kumachan-mis <29433058+kumachan-mis@users.noreply.github.com>
2024-11-04 00:31:19 +0900

    build: downgrade nextjs

--- src/app/(project)/projects/[projectId]/page.tsx
+++ src/app/(project)/projects/[projectId]/page.tsx
@@ -1,11 +1,17 @@
 import UnauthorizedError from '@/components/organisms/UnauthorizedError';
 
-import ProjectDetailPageClient, { ProjectDetailPageClientProps } from './client';
+import ProjectDetailPageClient from './client';
 
 import { getSession } from '@auth0/nextjs-auth0';
 import { NextPage } from 'next';
 
-const ProjectDetailPage: NextPage<ProjectDetailPageClientProps> = async (props) => {
+export type ProjectDetailPageProps = {
+  readonly params: {
+    readonly projectId: string;
+  };
+};
+
+const ProjectDetailPage: NextPage<ProjectDetailPageProps> = async (props) => {
   const session = await getSession();
   if (!session) {
     return <UnauthorizedError />;
