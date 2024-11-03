4ea647c84e97af34fb47b0c9be45b63a031864f5
kumachan-mis <29433058+kumachan-mis@users.noreply.github.com>
2024-11-03 23:49:16 +0900

    fix build error

--- src/app/(project)/projects/[projectId]/page.tsx
+++ src/app/(project)/projects/[projectId]/page.tsx
@@ -1,17 +1,23 @@
 import UnauthorizedError from '@/components/organisms/UnauthorizedError';
 
-import ProjectDetailPageClient, { ProjectDetailPageClientProps } from './client';
+import ProjectDetailPageClient from './client';
 
 import { getSession } from '@auth0/nextjs-auth0';
 import { NextPage } from 'next';
 
-const ProjectDetailPage: NextPage<ProjectDetailPageClientProps> = async (props) => {
+export type ProjectDetailPageProps = {
+  readonly params: Promise<{
+    readonly projectId: string;
+  }>;
+};
+
+const ProjectDetailPage: NextPage<ProjectDetailPageProps> = async ({ params }) => {
   const session = await getSession();
   if (!session) {
     return <UnauthorizedError />;
   }
 
-  return <ProjectDetailPageClient user={session.user} {...props} />;
+  return <ProjectDetailPageClient params={await params} user={session.user} />;
 };
 
 export default ProjectDetailPage;
